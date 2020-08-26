a6ce7b760cf22d3bee090b07f7fdec29ea369aa7
Martin Stoppler <ich@stoppe-gp.de>
2020-08-26 21:42:29 +0200

    remove obsolete files

--- ltbw_bot.py
+++
@@ -1,7 +0,0 @@
-from ltgetter import ltgetter
-from mattermost_adapter import mattermost_adapter
-from datetime import datetime
-from ltbw_bot_config import *
-
-ltgetter(database_name, start_date)
-mattermost_adapter(database_name, start_date)
